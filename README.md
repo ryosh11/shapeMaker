d000382bafa73cf42b70e55a49cae81e7fe3757b
Yoshanth Pranav Ramachandran <yoshanth.pranav@gmail.com>
2023-07-14 14:12:46 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+In this project you will tell the project what shape you want it to draw in a list of these five shapes included in the shapes.txt
+
+PLS tell me if I should add other shapes.
+
+PLS also contribute to my projects.
+
+THX
---
+++ shapemaker.py
@@ -0,0 +1,43 @@
+import turtle 
+
+t = turtle.Turtle()
+
+def square():
+    for i in range(4):
+        t.forward(100)
+        t.right(90)
+
+def rectangle():
+    for i in range(2):
+        t.forward(200)
+        t.right(90)
+        t.forward(100)
+        t.right(90)
+
+def circle():
+    t.circle(100)
+
+def triangle():
+    t.forward(100) 
+    for i in range(2):
+        t.left(120)
+        t.forward(100)
+
+def rhombus():
+    t.left(45)
+    t.forward(100)
+    for i in range(3):
+        t.left(90)
+        t.forward(100)
+
+name = input("What shape should I draw. ")
+if name == "square":
+    square()
+elif name == "rectangle":
+    rectangle()
+elif name == "triangle":
+    triangle()
+elif name == "circle":
+    circle()
+elif name == "rhombus":
+    rhombus()
